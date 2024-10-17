ebc85c437b172d43644714fdfed622ed57bff8e3
Yaroslav <aroslavirbahtin2@gmail.com>
2024-10-17 22:49:36 +0400

    снова небольшие правки (PEP 8).

--- Collections. Iterator. Generator..py
+++ Collections. Iterator. Generator..py
@@ -6,16 +6,16 @@ def filter_users(json_file, csv_file):
     with open(json_file, 'r') as f:
         users = json.load(f)
 
-    f_u = (
+    filtered_users = (
         user for user in users
         if (user.get('phoneNumber', '').startswith('+1') or user.get('phoneNumber', '').startswith('1'))
-            and '4.0 Safari' in user.get('userAgent', '')
+        and '4.0 Safari' in user.get('userAgent', '')
     )
 
     with open(csv_file, 'w', newline='') as f:
         writer = csv.writer(f)
 
-        for user in f_u:
+        for user in  filtered_users:
             writer.writerow([user.get('name'), user.get('address'), user.get('email')])
 
 
