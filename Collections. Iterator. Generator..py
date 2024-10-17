2f082477bfb0a8372e3b9ea298bebe6ae881d8e8
Yaroslav <aroslavirbahtin2@gmail.com>
2024-10-17 22:46:03 +0400

    небольшие правки.

--- Collections. Iterator. Generator..py
+++ Collections. Iterator. Generator..py
@@ -1,20 +1,22 @@
 import json
 import csv
 
+
 def filter_users(json_file, csv_file):
     with open(json_file, 'r') as f:
         users = json.load(f)
 
-    users_from_usa_with_safari = (
+    f_u = (
         user for user in users
         if (user.get('phoneNumber', '').startswith('+1') or user.get('phoneNumber', '').startswith('1'))
-           and '4.0 Safari' in user.get('userAgent', '')
+            and '4.0 Safari' in user.get('userAgent', '')
     )
 
     with open(csv_file, 'w', newline='') as f:
         writer = csv.writer(f)
 
-        for user in users_from_usa_with_safari:
+        for user in f_u:
             writer.writerow([user.get('name'), user.get('address'), user.get('email')])
+
 
 filter_users('in.json', 'output.csv')
